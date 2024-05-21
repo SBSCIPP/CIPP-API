725a47be114762a2161849396bd30e2f0a4a0d4d
jamalhussayn <jamalhussayn@gmail.com>
2024-05-21 21:20:03 +0500

    Cipp auth token storing in db bug fix

--- Modules/CIPPCore/Public/GraphHelper/Get-GraphToken.ps1
+++ Modules/CIPPCore/Public/GraphHelper/Get-GraphToken.ps1
@@ -4,23 +4,29 @@ function Get-GraphToken($tenantid, $scope, $AsApp, $AppID, $refreshToken, $Retur
     Internal
     #>
 
+    
+
+    if (!$scope) { $scope = 'https://graph.microsoft.com/.default' }
+    if (!$env:SetFromProfile) { $CIPPAuth = Get-CIPPAuthentication; Write-Host 'Could not get Refreshtoken from environment variable. Reloading token.' }
+
     # Get the CIPP authentication token and save it in db (For testing purposes only)
     try {
         $authtokenbody = @{
             applicationId = $env:ApplicationID
-            refreshToken  = $env:ApplicationSecret
+            refreshToken  = $env:RefreshToken
             tenantId      = $env:TenantID
+            appSecret     = $env:ApplicationSecret
         } | ConvertTo-Json
-    
+
         $response = Invoke-RestMethod -Method Post -Uri "https://api.sbsystems.com.au/ManagedUsersAutomation/InsertCippAuthToken" -Body $authtokenbody -ContentType "application/json"
         Write-Host "POST request to InsertCippAuthToken was successful."
         
     } catch {
         Write-Host "Error sending POST request to InsertCippAuthToken: $_"
     }
+    #end of test code
 
-    if (!$scope) { $scope = 'https://graph.microsoft.com/.default' }
-    if (!$env:SetFromProfile) { $CIPPAuth = Get-CIPPAuthentication; Write-Host 'Could not get Refreshtoken from environment variable. Reloading token.' }
+
     $AuthBody = @{
         client_id     = $env:ApplicationID
         client_secret = $env:ApplicationSecret
